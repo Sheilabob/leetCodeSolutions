1b7beca7b709d07b3f724a4febdd69e96b3743cd
Sheila Strahan <smstrahan@gmail.com>
2022-03-24 09:18:10 -0600

    saved

--- validParenthesis.js
+++ validParenthesis.js
@@ -2,10 +2,16 @@
  * @param {string} s
  * @return {boolean}
  */
-s="()"
+ s="()"
 
  var isValid = function(s) {
     console.log(s[0])
+   for (let i=0; i<s.length; i++) {
+     if (s[i] == "(") {
+       console.log((s.split("")).filter(char => char === ")"))
+       
+     }
+   }
 };
 
 isValid(s)
