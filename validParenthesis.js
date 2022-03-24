5dc1a1ba30951a11923a4a7ac90f3fe4e813a2c5
Sheila Strahan <smstrahan@gmail.com>
2022-03-24 15:27:29 -0600

    stuck for now, getting infinite in some cases

--- validParenthesis.js
+++ validParenthesis.js
@@ -5,13 +5,34 @@
  s="()"
 
  var isValid = function(s) {
-    console.log(s[0])
-   for (let i=0; i<s.length; i++) {
-     if (s[i] == "(") {
-       console.log((s.split("")).filter(char => char === ")"))
-       
+   
+   let sArray = s.split("")
+   
+  while (sArray.length>2) {
+   for (let i=0; i<sArray.length; i++) {
+  
+       if (sArray[i] == "(" && sArray[i+1] == ")") {
+         sArray.splice(i,2)
+         console.log(sArray)
+       } else if (sArray[i] == "{" && sArray[i+1] == "}"){
+         sArray.splice(i,2)
+         console.log(sArray)
+       } else if (sArray[i] == "[" && sArray[i+1] == "]") {
+         sArray.splice(i,2)
+         console.log(sArray)
+       } else {
+         console.log(sArray)
+       }
      }
    }
-};
-
-isValid(s)
+ 
+ console.log(sArray.length)
+   console.log(sArray.toString())
+ if (sArray.toString() === "(,)" || sArray.toString() === "[,]" || sArray.toString() === "{,}" ) {
+   return true
+ } else {
+   return false
+ }
+ };
+ 
+ isValid(s)
